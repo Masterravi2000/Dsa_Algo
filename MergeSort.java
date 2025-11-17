b97d964370852f32ced4b6cc8992fe6d9bd6045a
Masterravi2000 <raviqazqwerty123456@gmail.com>
2025-11-17 19:37:31 +0530

    Practiced & completed Merge sort with deep clarity of divide and conquer method

--- MergeSort.java
+++ MergeSort.java
@@ -1,8 +1,7 @@
-public class Practice {
-    public static void merge(int[] arr,int s, int mid, int e){
+public class MergeSort {
+    public static void merge(int[] arr,int s,int mid,int e){
         int[] L = new int[mid-s+1];
         int[] R = new int[e-mid];
-
         int k = 0;
         for(int i=s;i<=mid;i++){
             L[k] = arr[i];
@@ -17,57 +16,59 @@ public class Practice {
         int i = 0;
         int j = 0;
         k = s;
-        while( i < L.length && j < R.length){
+        while(i < L.length && j < R.length){
             if(L[i]>R[j]){
                 arr[k] = R[j];
-                k++;
                 j++;
+                k++;
             }
             else {
                 arr[k] = L[i];
-                k++;
                 i++;
+                k++;
             }
         }
-        while( i < L.length ){
+
+        while(i<L.length){
             arr[k] = L[i];
-            k++;
             i++;
-        }
-        while( j < R.length ){
-            arr[k] = R[j];
             k++;
+        }
+        while(j < R.length){
+            arr[k] = R[j];
             j++;
+            k++;
         }
     }
 
-    public static void mergeSort(int[] arr,int s,int e){
+    public static void mergeSort(int[] arr, int s, int e){
         if(s>=e) return;
         int mid = (s+e)/2;
 
-        //throw the left part into recursion for sorting 
+        //for Left
         mergeSort(arr,s,mid);
-        //throw the right pary into recursion for sorting
-        mergeSort(arr, mid+1,e);
-
+        //for Right
+        mergeSort(arr,mid+1,e);
         merge(arr,s,mid,e);
     }
-    public static void main(String[] args){
-        int[] arr = {2,6,8,4,0,1};
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 6, 3, 9, 7, 8, 1, 4 };
         int n = arr.length;
         int s = 0;
         int e = n-1;
-        System.out.println("Given array - ");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(" "+arr[i]);
+
+        System.out.print("Given array - ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
         }
-        System.out.println();
 
         mergeSort(arr,s,e);
 
-        System.out.println("Sorted array - ");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(" "+arr[i]);
+        System.out.println();
+        System.out.print("Sorted array using Merge sort - ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
         }
     }
 }
--- Q2.java
+++ Q2.java
@@ -3,7 +3,7 @@ public class Q2 {
         int n = arr.length;
         for(int i=0;i<n-1;i++){
             int min = i;
-            for(int j=i;j<n;j++){
+            for(int j=i+1;j<n;j++){
                 if(arr[min].compareTo(arr[j])>0){
                     min = j;
                 }
