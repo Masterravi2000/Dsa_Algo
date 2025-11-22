7f69396dd98a31221a949bf0e42bc8087bf49c10
Masterravi2000 <raviqazqwerty123456@gmail.com>
2025-11-22 12:07:27 +0530

    Practiced Quick Sort again

--- Practice.java
+++ Practice.java
@@ -1,59 +1,56 @@
 public class Practice {
-    public static void merge(int[] arr,int s,int mid, int e){
-        //create actual two halves Left and Right
-        int[] L = new int[mid-s+1];
-        int[] R = new int[e-mid];
-        int k = 0;
-        for(int i=s;i<=mid;i++){
-            L[k] = arr[i];
-            k++;
-        }
-        k = 0;
-        for(int i=mid+1;i<=e;i++){
-            R[k] = arr[i];
-            k++;
-        }
-
-        //3 different pointers i for L, j for R and k for arr
-        int i = 0;
-        int j = 0;
-        k = s;
-        while(i<L.length && j<R.length){
-            if(L[i]>R[j]){
-                arr[k] = R[j];
-                k++;
-                j++;
+    public static int particision(int[] arr,int s, int e){
+        //first hold a value as pivot let say k
+        int count = 0;
+        //now place this kth value to its correct position
+        for(int i=s+1;i<=e;i++){
+            //now count the smaller number from the holded pivot
+            if(arr[s]>=arr[i]){
+                count++;
             }
-            else {
-                arr[k] = L[i];
-                k++;
+        }
+        //now place the holded pivvot k after total counted value which will be the pivot index
+        int p = s+count;
+        //now swap the kth and pivot index values in array
+        int t = arr[s];
+        arr[s] = arr[p];
+        arr[p] = t;
+
+        //now bring all the smaller values in the right of the pivot and all the greater value in the left of the pivot
+        int i = s;
+        int j = e;
+        while(i <= p && j >= p){
+            if(arr[i]>=arr[p] && arr[j]<=arr[p]){
+                //swap
+                int t2 = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t2;
+                i++;
+                j--;
+            }
+            else if(arr[i]>=arr[p]){
+                j--;
+            }
+            else if(arr[j]<=arr[p]){
                 i++;
             }
+            else {
+                i++;
+                j--;
+            }
         }
-        while(i<L.length){
-            arr[k] = L[i];
-            k++;
-            i++;
-        }
-        while(j<R.length){
-            arr[k] = R[j];
-            k++;
-            j++;
-        }
+        return p;
     }
-    public static void mergeSort(int[] arr,int s,int e){
-        //base case for stoping the recursion
+    public static void QuickSort(int[] arr,int s,int e){
+        //base case for stoping the recusion
         if(s>=e) return;
-        //divide the array into two equal halves
-        int mid = (s+e)/2;
+        //find the pivot value
+        int p = particision(arr,s,e);
 
-        //throw left part into the recursion for sorting
-        mergeSort(arr,s,mid);
-        //now throw the right part into the recursion for sorting
-        mergeSort(arr, mid+1, e);
-
-        //now merge both sorted left and right part
-        merge(arr,s,mid,e);
+        //now throw the left part into recursion for sorting
+        QuickSort(arr,s,p-1);
+        //now throw the right part into the recusion for sorting
+        QuickSort(arr,p+1,e);
     }
     public static void main(String[] args){
         int[] arr = {8,7,6,5,4,3,2,1};
@@ -66,7 +63,7 @@ public class Practice {
         }
         System.out.println();
 
-        mergeSort(arr,s,e);
+        QuickSort(arr,s,e);
 
         System.out.println("Sorted array - ");
         for(int i=0;i<arr.length;i++){
