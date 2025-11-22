917fc959ff33fc47adf0a044a9b849224273d42a
Masterravi2000 <raviqazqwerty123456@gmail.com>
2025-11-22 11:41:13 +0530

    merge sort Practiced again

--- Practice.java
+++ Practice.java
@@ -1,71 +1,75 @@
 public class Practice {
-    //creating the swap method in order to eliminate repetations
-    public static void swap(int[] arr,int i,int j){
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
-    public static int particion(int[] arr,int s,int e){
-        int count = 0;
-        //now bring this in its correct place
-        for(int i=s+1;i<=e;i++){
-            if(arr[s]>=arr[i]){
-                count++;
-            }
+    public static void merge(int[] arr,int s,int mid, int e){
+        //create actual two halves Left and Right
+        int[] L = new int[mid-s+1];
+        int[] R = new int[e-mid];
+        int k = 0;
+        for(int i=s;i<=mid;i++){
+            L[k] = arr[i];
+            k++;
         }
-        int p = s+count;
-        //now Swap to place the pivot to its right place
-        swap(arr,p,s);
+        k = 0;
+        for(int i=mid+1;i<=e;i++){
+            R[k] = arr[i];
+            k++;
+        }
 
-        //now bring the smaller element to the left and larger element to the right
-        int i = s;
-        int j = e;
-        while(i<=p && p<=j){
-            if(arr[i] > arr[p] && arr[j] < arr[p]){
-                //swap
-                swap(arr,i,j);
-                i++;
-                j--;
-            }
-            else if(arr[i] > arr[p]){
-                j--;
-            }
-            else if(arr[j] < arr[p]){
-                i++;
+        //3 different pointers i for L, j for R and k for arr
+        int i = 0;
+        int j = 0;
+        k = s;
+        while(i<L.length && j<R.length){
+            if(L[i]>R[j]){
+                arr[k] = R[j];
+                k++;
+                j++;
             }
             else {
+                arr[k] = L[i];
+                k++;
                 i++;
-                j--;
             }
         }
-
-        return p;
-
+        while(i<L.length){
+            arr[k] = L[i];
+            k++;
+            i++;
+        }
+        while(j<R.length){
+            arr[k] = R[j];
+            k++;
+            j++;
+        }
     }
-    public static void QuickSort(int[] arr,int s,int e){
+    public static void mergeSort(int[] arr,int s,int e){
+        //base case for stoping the recursion
         if(s>=e) return;
-        int p = particion(arr,s,e);
+        //divide the array into two equal halves
+        int mid = (s+e)/2;
 
-        //now throw the left part into the recursion to sort
-        QuickSort(arr,s,p-1);
-        //and further throw the right part into recursion to sort
-        QuickSort(arr,p+1,e);
+        //throw left part into the recursion for sorting
+        mergeSort(arr,s,mid);
+        //now throw the right part into the recursion for sorting
+        mergeSort(arr, mid+1, e);
+
+        //now merge both sorted left and right part
+        merge(arr,s,mid,e);
     }
     public static void main(String[] args){
-        int[] arr = {7,13,8,5,10,4,2};
+        int[] arr = {8,7,6,5,4,3,2,1};
         int n = arr.length;
         int s = 0;
         int e = n-1;
-        System.out.println("Given array -");
-        for(int i=0;i<n;i++){
+        System.out.println("Given array - ");
+        for(int i=0;i<arr.length;i++){
             System.out.print(" "+arr[i]);
         }
         System.out.println();
 
-        QuickSort(arr,s,e);
+        mergeSort(arr,s,e);
 
-        System.out.println("Sorted Array - ");
-        for(int i=0;i<n;i++){
+        System.out.println("Sorted array - ");
+        for(int i=0;i<arr.length;i++){
             System.out.print(" "+arr[i]);
         }
     }
