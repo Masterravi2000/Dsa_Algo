28f02f711db609a8ed94d0431bfd37ea1c067ffe
Masterravi2000 <raviqazqwerty123456@gmail.com>
2025-11-27 23:59:57 +0530

    done

--- Practice.java
+++ Practice.java
@@ -1,37 +1,26 @@
 public class Practice {
-    public static int binarySearch(int[] arr,int t,int s,int e){
-        int mid = (s+e)/2;
-        //base condition
-        if(s>e){
-            return arr[mid];
-        }
-        if(arr[mid]==t){
-            return arr[mid];
-        }
-        else if(arr[mid]<t){
-            return binarySearch(arr,t,mid+1,e);
-        }
-        else if(arr[mid]>t){
-            return binarySearch(arr,t,s,mid-1);
-        }else {
-            return -1;
-        }
+    public static int binarySearch(int[] arr,int s,int e){
+        //find the mid
+        int mid = s+(e-s)/2;
+
+        // if(arr[mid])
+        return arr[0];
     }
     public static void main(String[] args){
-        int[] arr = {2,4,5,7,15,20,24,45,50,77};
+        int[] arr = {2,6,9,5,3,1,7};
         int n = arr.length;
-        int t = 24;
+        int t = 9;
         int s = 0;
-        int e = n-1;
-
-        System.out.println("Given Array - ");
-        for(int i=0;i<n;i++){
+        int e = n-1
+        System.out.println("Given array -");
+        for(int i=0;i<arr.length;i++){
             System.out.print(" "+arr[i]);
         }
         System.out.println();
 
-        System.out.println("Find the given target from this sorted array - ");
-        int target = binarySearch(arr,t,s,e);
-        System.out.println("Got the target using Binary Search - "+target);
+        int target = binarySearch(arr,s,e);
+
+        System.out.println("Target is - "+t);
+        System.out.println("Target found using binary search - "+target);
     }
 }
